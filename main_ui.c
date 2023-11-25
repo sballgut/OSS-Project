71d83b5410268fb454e454a78c038a56a44f54a7
sunghyun <128571392+sballgut@users.noreply.github.com>
2023-11-25 14:07:25 +0900

    Update main_ui.c
    SelectGame 함수 추가

--- main_ui.c
+++ main_ui.c
@@ -10,9 +10,11 @@ void gotoxy(int x, int y);
 void SetConsole();
 void textcolor(int color_number);
 int getKeyDown();
+void SelectGame();
 
 int main() {
 	int keyInput = getKeyDown();
+	int state = 0;
 	SetConsole();
 	gotoxy(8, 0);
 	printf("추억의 미니게임");
@@ -27,16 +29,27 @@ int main() {
 	
 	while (1) {
 		if (GetAsyncKeyState(VK_LEFT)) {
+			state = 1;
 			gotoxy(15, 6);
 			printf(" ");
 			gotoxy(3, 6);
 			printf(">");
 		}
 		else if (GetAsyncKeyState(VK_RIGHT)) {
+			state = 2;
 			gotoxy(3, 6);
 			printf(" ");
 			gotoxy(15, 6);
 			printf(">");
+		}
+		else if (GetAsyncKeyState(VK_RETURN)) {
+			if (state == 1) {
+				SelectGame();
+			}
+			else if (state == 2) {
+				gotoxy(0, 20);
+				break;
+			}
 		}
 	}
 	return 0;
@@ -68,4 +81,61 @@ void textcolor(int color_number) {
 int getKeyDown() {
 	if (_kbhit()) return _getch();
 	else return -1;
+}
+
+void SelectGame() {
+	int x;
+	x = 5;
+	system("cls");
+	system("mode con: cols=70 lines=30");
+	gotoxy(18, 1);
+	printf("게임 선택");
+	gotoxy(0, 3);
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+	printf("■                                          ■\n");
+	printf("■                                          ■\n");
+	printf("■      숫자야구    n*n퍼즐     테트리스    ■\n");
+	printf("■                                          ■\n");
+	printf("■                                          ■\n");
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
+	gotoxy(x, 6);
+	printf(">");
+	while (1) {
+		if (GetAsyncKeyState(VK_LEFT)) {
+			if (x <= 5 && GetAsyncKeyState(VK_LEFT)) {
+				gotoxy(5, 6);
+				printf(" ");
+				gotoxy(5, 6);
+				printf(">");
+			}
+			gotoxy(x, 6);
+			printf(" ");
+			x -= 12;
+			gotoxy(x, 6);
+			printf(">");
+		}
+		else if (GetAsyncKeyState(VK_RIGHT)) {
+			if (x >= 29 && GetAsyncKeyState(VK_RIGHT)) {
+				gotoxy(29, 6);
+				printf(" ");
+				gotoxy(29, 6);
+				printf(">");
+			}
+			gotoxy(x, 6);
+			printf(" ");
+			x += 12;
+			gotoxy(x, 6);
+			printf(">");
+		}
+		if (x == 5) {
+
+		}
+		else if (x == 17) {
+
+		}
+		else if (x == 29) {
+
+		}
+		Sleep(200);
+	}
 }
