df3e6d9887a63abd3fffb500365aaef1195dd395
sunghyun <128571392+sballgut@users.noreply.github.com>
2023-12-10 00:34:02 +0900

    Update 추억의 미니게임.c
    n*n퍼즐 코드 합치기 완료

--- 추억의 미니게임.c
+++ 추억의 미니게임.c
@@ -4,7 +4,13 @@
 #include <Windows.h>
 #include <stdio.h>
 #include <conio.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
+//n*n퍼즐, 테트리스 공용 상
 #define UP 72
 #define LEFT 75
 #define RIGHT 77
@@ -36,6 +42,19 @@ struct Player {
 };
 
 //n*n퍼즐 함수 선언
+void NPuzzle();
+void explainNPuzzle();
+void printfui();
+int EgetDirectionKey();
+void EprintPuzzle(int puzzle[][3]);
+int EisEnding(int puzzle[][3]);
+void EshufflePuzzle(int puzzle[][3]);
+int easypuzzle();
+int HgetDirectionKey();
+void HprintPuzzle(int puzzle[][4]);
+int HisEnding(int puzzle[][4]);
+void HshufflePuzzle(int puzzle[][PUZZLE_SIZE]);
+int hardpuzzle();
 
 //main_ui 함수 선언
 void gotoxy(int x, int y);
@@ -72,38 +91,41 @@ void main_ui() {
 
 
 	while (1) {
+		Sleep(100);
 		if (state == 1) {
 			gotoxy(5, 6);
 			textcolor(4);
 			printf("게임시작");
 		}
-
+		
+		else if (state == 2) {
+			gotoxy(17, 6);
+			textcolor(1);
+			printf("게임종료");
+		}
+		
 		if (state == 1 && GetAsyncKeyState(VK_RIGHT)) {
 			state = 2;
 			gotoxy(5, 6);
 			textcolor(7);
 			printf("게임시작");
 		}
-
-		if (state == 2) {
-			gotoxy(17, 6);
-			textcolor(1);
-			printf("게임종료");
-		}
-
-		if (state == 2 && GetAsyncKeyState(VK_LEFT)) {
+		
+		else if (state == 2 && GetAsyncKeyState(VK_LEFT)) {
 			state = 1;
 			gotoxy(17, 6);
 			textcolor(7);
 			printf("게임종료");
 		}
-
+		
 		if (state == 1 && GetAsyncKeyState(VK_UP)) {
 			SelectGame();
 			break;
 		}
-
-		if (state == 2 && GetAsyncKeyState(VK_UP)) {
+		
+		else if (state == 2 && GetAsyncKeyState(VK_UP)) {
+			gotoxy(0, 20);
+			textcolor(7);
 			break;
 		}
 	}
@@ -142,6 +164,7 @@ void SelectGame() {
 	int game = 1;
 	system("cls");
 	system("mode con: cols=70 lines=30");
+	textcolor(7);
 	Sleep(200);
 	gotoxy(18, 1);
 	printf("게임 선택");
@@ -153,59 +176,71 @@ void SelectGame() {
 	printf("■                                          ■\n");
 	printf("■                                          ■\n");
 	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
-	gotoxy(6, 6);
-	textcolor(4);
-	printf("숫자야구");
+	
 	while (1) {
 		Sleep(100);
+		if (game == 1) {
+			gotoxy(7, 6);
+			textcolor(4);
+			printf("숫자야구");
+		}
+		
+		else if (game == 2) {
+			gotoxy(19, 6);
+			textcolor(6);
+			printf("n*n퍼즐");
+		}
+		
+		else if (game == 3) {
+			gotoxy(31, 6);
+			textcolor(1);
+			printf("테트리스");
+		}
+		
 		if (game == 1 && GetAsyncKeyState(VK_RIGHT)) {
 			game = 2;
 			gotoxy(6, 6);
 			textcolor(7);
 			printf("숫자야구");
-			gotoxy(19, 6);
-			textcolor(6);
-			printf("n*n퍼즐");
 		}
-
-		if (game == 2 && GetAsyncKeyState(VK_LEFT)) {
+		
+		else if (game == 2 && GetAsyncKeyState(VK_LEFT)) {
 			game = 1;
 			gotoxy(19, 6);
 			textcolor(7);
 			printf("n*n퍼즐");
-			gotoxy(6, 6);
-			textcolor(4);
-			printf("숫자야구");
 		}
-
-		if (game == 2 && GetAsyncKeyState(VK_RIGHT)) {
+		
+		else if (game == 2 && GetAsyncKeyState(VK_RIGHT)) {
 			game = 3;
 			gotoxy(19, 6);
 			textcolor(7);
 			printf("n*n퍼즐");
-			gotoxy(31, 6);
-			textcolor(1);
-			printf("테트리스");
 		}
-
-		if (game == 3 && GetAsyncKeyState(VK_LEFT)) {
+		
+		else if (game == 3 && GetAsyncKeyState(VK_LEFT)) {
 			game = 2;
 			gotoxy(31, 6);
 			textcolor(7);
 			printf("테트리스");
-			gotoxy(19, 6);
-			textcolor(6);
-			printf("n*n퍼즐");
 		}
-
+		
+		//숫자야구 선택
 		if (game == 1 && GetAsyncKeyState(VK_UP)) {
 			StartNumberBaseball();
+			break;
 		}
+		
+		//n*n퍼즐 선택
 		else if (game == 2 && GetAsyncKeyState(VK_UP)) {
 			StartNPuzzle();
+			break;
 		}
+		
+		//테트리스 선택
 		else if (game == 3 && GetAsyncKeyState(VK_UP)) {
 			StartTetris();
+			break;
 		}
 	}
 }
@@ -234,65 +269,67 @@ void StartNumberBaseball() {
 			printf("게임시작");
 		}
 
+		else if (state == 2) {
+			gotoxy(20, 6);
+			textcolor(6);
+			printf("게임설명");
+		}
+		
+		else if (state == 3) {
+			gotoxy(35, 6);
+			textcolor(1);
+			printf("홈으로");
+		}
+		
 		if (state == 1 && GetAsyncKeyState(VK_RIGHT)) {
 			state = 2;
 			gotoxy(5, 6);
 			textcolor(7);
 			printf("게임시작");
 		}
-
-		if (state == 2) {
-			gotoxy(20, 6);
-			textcolor(6);
-			printf("게임설명");
-		}
-
-		if (state == 2 && GetAsyncKeyState(VK_LEFT)) {
+		
+		
+		else if (state == 2 && GetAsyncKeyState(VK_LEFT)) {
 			state = 1;
 			gotoxy(20, 6);
 			textcolor(7);
 			printf("게임설명");
 		}
-
-		if (state == 2 && GetAsyncKeyState(VK_RIGHT)) {
+		
+		else if (state == 2 && GetAsyncKeyState(VK_RIGHT)) {
 			state = 3;
 			gotoxy(20, 6);
 			textcolor(7);
 			printf("게임설명");
 		}
-
-		if (state == 3) {
-			gotoxy(35, 6);
-			textcolor(1);
-			printf("홈으로");
-		}
-
-		if (state == 3 && GetAsyncKeyState(VK_LEFT)) {
+		
+		else if (state == 3 && GetAsyncKeyState(VK_LEFT)) {
 			state = 2;
 			gotoxy(35, 6);
 			textcolor(7);
 			printf("홈으로");
 		}
-
+		
+		//숫자야구 실행
 		if (state == 1 && GetAsyncKeyState(VK_UP)) {
 			gotoxy(0, 0);
 			NumBaseball();
 			break;
 		}
-
-		if (state == 2 && GetAsyncKeyState(VK_UP)) {
+		
+		//숫자야구 게임 설명
+		else if (state == 2 && GetAsyncKeyState(VK_UP)) {
 			gotoxy(0, 0);
 			explainNumberBaseball();
 			break;
 		}
-
-		if (state == 3 && GetAsyncKeyState(VK_UP)) {
+		
+		//첫화면으로
+		else if (state == 3 && GetAsyncKeyState(VK_UP)) {
 			gotoxy(0, 0);
 			main_ui();
 			break;
 		}
-
-		Sleep(100);
 	}
 }
 
@@ -320,65 +357,66 @@ void StartNPuzzle() {
 			printf("게임시작");
 		}
 
+		else if (state == 2) {
+			gotoxy(20, 6);
+			textcolor(6);
+			printf("게임설명");
+		}
+		
+		else if (state == 3) {
+			gotoxy(35, 6);
+			textcolor(1);
+			printf("홈으로");
+		}
+		
 		if (state == 1 && GetAsyncKeyState(VK_RIGHT)) {
 			state = 2;
 			gotoxy(5, 6);
 			textcolor(7);
 			printf("게임시작");
 		}
-
-		if (state == 2) {
-			gotoxy(20, 6);
-			textcolor(6);
-			printf("게임설명");
-		}
-
-		if (state == 2 && GetAsyncKeyState(VK_LEFT)) {
+		
+		else if (state == 2 && GetAsyncKeyState(VK_LEFT)) {
 			state = 1;
 			gotoxy(20, 6);
 			textcolor(7);
 			printf("게임설명");
 		}
-
-		if (state == 2 && GetAsyncKeyState(VK_RIGHT)) {
+		
+		else if (state == 2 && GetAsyncKeyState(VK_RIGHT)) {
 			state = 3;
 			gotoxy(20, 6);
 			textcolor(7);
 			printf("게임설명");
 		}
-
-		if (state == 3) {
-			gotoxy(35, 6);
-			textcolor(1);
-			printf("홈으로");
-		}
-
-		if (state == 3 && GetAsyncKeyState(VK_LEFT)) {
+		
+		else if (state == 3 && GetAsyncKeyState(VK_LEFT)) {
 			state = 2;
 			gotoxy(35, 6);
 			textcolor(7);
 			printf("홈으로");
 		}
-
+		
+		//n*n퍼즐 실행
 		if (state == 1 && GetAsyncKeyState(VK_UP)) {
 			gotoxy(0, 0);
-			NumBaseball();
+			NPuzzle();
 			break;
 		}
-
-		if (state == 2 && GetAsyncKeyState(VK_UP)) {
+		
+		//n*n퍼즐 게임 설명
+		else if (state == 2 && GetAsyncKeyState(VK_UP)) {
 			gotoxy(0, 0);
-			explainNumberBaseball();
+			explainNPuzzle();
 			break;
 		}
-
-		if (state == 3 && GetAsyncKeyState(VK_UP)) {
+		
+		//첫화면으로
+		else if (state == 3 && GetAsyncKeyState(VK_UP)) {
 			gotoxy(0, 0);
 			main_ui();
 			break;
 		}
-
-		Sleep(100);
 	}
 }
 
@@ -406,69 +444,70 @@ void StartTetris() {
 			printf("게임시작");
 		}
 
+		else if (state == 2) {
+			gotoxy(20, 6);
+			textcolor(6);
+			printf("게임설명");
+		}
+		
+		else if (state == 3) {
+			gotoxy(35, 6);
+			textcolor(1);
+			printf("홈으로");
+		}
+		
 		if (state == 1 && GetAsyncKeyState(VK_RIGHT)) {
 			state = 2;
 			gotoxy(5, 6);
 			textcolor(7);
 			printf("게임시작");
 		}
-
-		if (state == 2) {
-			gotoxy(20, 6);
-			textcolor(6);
-			printf("게임설명");
-		}
-
-		if (state == 2 && GetAsyncKeyState(VK_LEFT)) {
+		
+		else if (state == 2 && GetAsyncKeyState(VK_LEFT)) {
 			state = 1;
 			gotoxy(20, 6);
 			textcolor(7);
 			printf("게임설명");
 		}
-
-		if (state == 2 && GetAsyncKeyState(VK_RIGHT)) {
+		
+		else if (state == 2 && GetAsyncKeyState(VK_RIGHT)) {
 			state = 3;
 			gotoxy(20, 6);
 			textcolor(7);
 			printf("게임설명");
 		}
-
-		if (state == 3) {
-			gotoxy(35, 6);
-			textcolor(1);
-			printf("홈으로");
-		}
-
-		if (state == 3 && GetAsyncKeyState(VK_LEFT)) {
+		
+		else if (state == 3 && GetAsyncKeyState(VK_LEFT)) {
 			state = 2;
 			gotoxy(35, 6);
 			textcolor(7);
 			printf("홈으로");
 		}
-
+		
+		//테트리스 실행
 		if (state == 1 && GetAsyncKeyState(VK_UP)) {
 			gotoxy(0, 0);
-			NumBaseball();
+			Tetris();
 			break;
 		}
-
-		if (state == 2 && GetAsyncKeyState(VK_UP)) {
+		
+		//테트리스 게임 설명
+		else if (state == 2 && GetAsyncKeyState(VK_UP)) {
 			gotoxy(0, 0);
-			explainNumberBaseball();
+			explainTetris();
 			break;
 		}
-
-		if (state == 3 && GetAsyncKeyState(VK_UP)) {
+		
+		//첫화면으로
+		else if (state == 3 && GetAsyncKeyState(VK_UP)) {
 			gotoxy(0, 0);
 			main_ui();
 			break;
 		}
-
-		Sleep(100);
 	}
 }
 
-//숫자야구
+//숫자야구 메인 함수
 void NumBaseball() {
 	system("cls");
 	textcolor(7);
@@ -856,3 +895,402 @@ void explainNumberBaseball() {
 	printf("게임 종료 후 자신의 이름을 입력하면 랭킹에 등록됩니다.");
 }
 //숫자야구 함수 끝
+
+//n*n퍼즐 메인 함수
+void NPuzzle() {
+	system("cls");
+	textcolor(7);
+	clock_t start_time, end_time;
+	double cpu_time_used;
+
+	start_time = clock();  // 시작 시간 기록
+
+	// 실행 시간을 측정하고자 하는 작업
+	easypuzzle();
+
+	end_time = clock();  // 끝 시간 기록
+
+	// 실행 시간 계산 (초 단위)
+	cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+	printf("Elapsed CPU time: %.6f seconds\n", cpu_time_used);
+
+	int choose;
+
+	if (cpu_time_used > 30) {
+		printf("      But game out(30초를 넘기셨습니다)\n");
+		printf("            게임을 다시하겠습니까?\n\n");
+		printf("              1.YES    2.NO\n");
+		scanf_s("%d", &choose);
+		if (choose == 1) {
+			for (; cpu_time_used > 30;) {
+				start_time = clock();  // 시작 시간 기록
+
+				// 실행 시간을 측정하고자 하는 작업
+				easypuzzle();
+
+				end_time = clock();  // 끝 시간 기록
+				cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+
+			}
+			hardpuzzle();
+		}
+
+	}
+	else if (cpu_time_used < 30) {
+		gotoxy(100, 15);
+		printf("\n");
+		printfui();
+
+#ifdef _WIN32
+		Sleep(9000); // Windows
+		system("cls");
+#else
+
+
+#endif
+
+		hardpuzzle();
+	}
+
+
+	return 0;
+}
+
+//n*n퍼즐 함수
+void printfui() {
+	printf("                                                              *****\n");
+	printf("      ***      *    ********       *        *****      **             **\n");
+	printf("    *     *    *    *             * *       *     *              **        ***\n");
+	printf("   *           *    ********     *   *      *****           **    ***       **\n");
+	printf("    *     *    *    *           *******     *    *      **       *      ***    ***\n");
+	printf("      ***      *    ********   *       *    *     *                        ****   ***\n");
+	printf("                                                              **   ****  *****    *  *\n");
+	printf("                                                          ***     **      ***  *      *\n");
+	printf("                                                     *                      * **********\n");
+	printf("                                                                              *         *\n");
+	printf("                                                                                **********\n");
+	printf("                                                                                  *      *\n");
+	printf("                                                                                     * *\n");
+	printf("                                                                                ");
+
+}
+
+int EgetDirectionKey() {
+	int key = _getch();
+	if (key == 224) {
+		return _getch();
+	}
+	return 0;
+}
+
+void EprintPuzzle(int puzzle[][3]) {
+
+	system("cls");
+	printf("\n\n");
+	printf("■■■■■■■■■■■■■\n");
+	for (int r = 0; r < 3; r++) {
+		for (int c = 0; c < 3; c++) {
+			printf("■");
+			if (puzzle[r][c]) {
+				// 각 숫자를 출력할 때 일정한 간격을 확보
+				if (puzzle[r][c] < 10) {
+					printf("   %d  ", puzzle[r][c]);
+				}
+				else {
+					printf("    %d   ", puzzle[r][c]);
+				}
+			}
+			else {
+				// 빈 칸은 공백 출력
+				printf("      ");
+			}
+		}
+		printf("■\n");
+		if (r < 2) {
+			for (int c = 0; c < 1; c++) {
+				printf("■■■■■■■■■■■■■");
+			}
+			printf("\n");
+		}
+
+	}
+	printf("■■■■■■■■■■■■■\n\n");
+
+	gotoxy(28, 4);
+	printf("    ■■■■■\n");
+	gotoxy(28, 5);
+	printf("    ■ 3*3  ■\n");
+	gotoxy(28, 6);
+	printf("    ■■■■■\n");
+}
+
+int EisEnding(int puzzle[][3]) {
+	int count = 1;
+	for (int r = 0; r < 3; r++) {
+		for (int c = 0; c < 3; c++) {
+			if (puzzle[r][c] != count) {
+				return 0;
+			}
+			count = (count == 8) ? 0 : count + 1;
+		}
+	}
+	return 1;
+}
+
+void EshufflePuzzle(int puzzle[][3]) {
+	srand(time(NULL));
+
+	int flatPuzzle[9];
+	for (int i = 0; i < 8; i++) {
+		flatPuzzle[i] = i + 1;
+	}
+	flatPuzzle[8] = 0;  // 0은 빈 칸을 나타냄
+
+	for (int i = 8; i > 0; i--) {
+		int j = rand() % (i + 1);
+		int temp = flatPuzzle[i];
+		flatPuzzle[i] = flatPuzzle[j];
+		flatPuzzle[j] = temp;
+	}
+
+	for (int r = 0; r < 3; r++) {
+		for (int c = 0; c < 3; c++) {
+			puzzle[r][c] = flatPuzzle[r * 3 + c];
+		}
+	}
+}
+
+int easypuzzle() {
+	srand(time(NULL));
+
+	// 1부터 10까지의 난수 생성
+	int randomnumber = rand() % 5 + 1;
+
+	int puzzle[3][3];
+	if (randomnumber == 1) {
+		puzzle[0][0] = 8; puzzle[0][1] = 3; puzzle[0][2] = 5;
+		puzzle[1][0] = 1; puzzle[1][1] = 4; puzzle[1][2] = 6;
+		puzzle[2][0] = 2; puzzle[2][1] = 7; puzzle[2][2] = 0;
+	}
+	else if (randomnumber == 2) {
+		puzzle[0][0] = 7; puzzle[0][1] = 6; puzzle[0][2] = 3;
+		puzzle[1][0] = 8; puzzle[1][1] = 1; puzzle[1][2] = 2;
+		puzzle[2][0] = 5; puzzle[2][1] = 4; puzzle[2][2] = 0;
+	}
+	else if (randomnumber == 3) {
+		puzzle[0][0] = 2; puzzle[0][1] = 3; puzzle[0][2] = 4;
+		puzzle[1][0] = 1; puzzle[1][1] = 7; puzzle[1][2] = 8;
+		puzzle[2][0] = 6; puzzle[2][1] = 5; puzzle[2][2] = 0;
+	}
+	else if (randomnumber == 4) {
+		puzzle[0][0] = 2; puzzle[0][1] = 7; puzzle[0][2] = 6;
+		puzzle[1][0] = 3; puzzle[1][1] = 4; puzzle[1][2] = 8;
+		puzzle[2][0] = 1; puzzle[2][1] = 5; puzzle[2][2] = 0;
+	}
+	else if (randomnumber == 5) {
+		puzzle[0][0] = 8; puzzle[0][1] = 3; puzzle[0][2] = 1;
+		puzzle[1][0] = 2; puzzle[1][1] = 4; puzzle[1][2] = 7;
+		puzzle[2][0] = 6; puzzle[2][1] = 5; puzzle[2][2] = 0;
+	}
+
+	int row, col;
+	for (int r = 0; r < 3; r++) {
+		for (int c = 0; c < 3; c++) {
+			if (puzzle[r][c] == 0) {
+				row = r;
+				col = c;
+			}
+		}
+	}
+
+	int key = 0;
+
+	while (!EisEnding(puzzle)) {
+		EprintPuzzle(puzzle);
+		printf("\n\n\n\n");
+		printf(">> 방향키 선택\n");
+		key = EgetDirectionKey();
+
+		switch (key) {
+		case RIGHT:
+			if (col > 0) {
+				puzzle[row][col] = puzzle[row][col - 1];
+				puzzle[row][col - 1] = 0;
+				col--;
+			}
+			break;
+			// LEFT 방향 이동
+		case LEFT:
+			if (col < 2) {
+				puzzle[row][col] = puzzle[row][col + 1];
+				puzzle[row][col + 1] = 0;
+				col++;
+			}
+			break;
+			// UP 방향 이동
+		case UP:
+			if (row < 2) {
+				puzzle[row][col] = puzzle[row + 1][col];
+				puzzle[row + 1][col] = 0;
+				row++;
+			}
+			break;
+			// DOWN 방향 이동
+		case DOWN:
+			if (row > 0) {
+				puzzle[row][col] = puzzle[row - 1][col];
+				puzzle[row - 1][col] = 0;
+				row--;
+			}
+			break;
+		}
+	}
+
+	printf("게임 클리어!\n");
+	return 0;
+}
+
+int HgetDirectionKey() {
+	int key = _getch();
+	if (key == 224) {
+		return _getch();
+	}
+	return 0;
+}
+
+void HprintPuzzle(int puzzle[][4]) {
+	system("cls");
+	printf("\n\n");
+	printf("■■■■■■■■■■■■■■■\n");
+	for (int r = 0; r < 4; r++) {
+		for (int c = 0; c < 4; c++) {
+			printf("■");
+			if (puzzle[r][c] < 10 && puzzle[r][c] > 0) {
+				// 각 숫자를 출력할 때 일정한 간격을 확보
+				printf("  %d  ", puzzle[r][c]);
+			}
+			else if (puzzle[r][c] > 9 && puzzle[r][c] < 16) {
+				printf(" %d  ", puzzle[r][c]);
+			}
+			else {
+				// 빈 칸은 공백으로 출력합니다.
+				printf("     ");
+			}
+		}
+		printf("■\n");
+		// 세로선 출력
+		if (r < 3) {
+			printf("■■■■■■■■■■■■■■■\n");
+		}
+		else {
+			printf("■■■■■■■■■■■■■■■\n");
+		}
+	}
+	gotoxy(35, 5);
+	printf("    ■■■■■\n");
+	gotoxy(35, 6);
+	printf("    ■ 4*4  ■\n");
+	gotoxy(35, 7);
+	printf("    ■■■■■\n");
+}
+
+
+
+int HisEnding(int puzzle[][4]) {
+	int count = 1;
+	for (int r = 0; r < PUZZLE_SIZE; r++) {
+		for (int c = 0; c < PUZZLE_SIZE; c++) {
+			if (puzzle[r][c] != count) {
+				return 0;
+			}
+			count = (count == PUZZLE_SIZE * PUZZLE_SIZE - 1) ? 0 : count + 1;
+		}
+	}
+	return 1;
+}
+
+void HshufflePuzzle(int puzzle[][PUZZLE_SIZE]) {
+	srand(time(NULL));
+
+	int flatPuzzle[PUZZLE_SIZE * PUZZLE_SIZE];
+	for (int i = 0; i < PUZZLE_SIZE * PUZZLE_SIZE - 1; i++) {
+		flatPuzzle[i] = i + 1;
+	}
+	flatPuzzle[PUZZLE_SIZE * PUZZLE_SIZE - 1] = 0;  // 0은 빈 칸을 나타냄
+
+	for (int i = PUZZLE_SIZE * PUZZLE_SIZE - 1; i > 0; i--) {
+		int j = rand() % (i + 1);
+		int temp = flatPuzzle[i];
+		flatPuzzle[i] = flatPuzzle[j];
+		flatPuzzle[j] = temp;
+	}
+
+	for (int r = 0; r < PUZZLE_SIZE; r++) {
+		for (int c = 0; c < PUZZLE_SIZE; c++) {
+			puzzle[r][c] = flatPuzzle[r * PUZZLE_SIZE + c];
+		}
+	}
+}
+
+int hardpuzzle() {
+	int puzzle[PUZZLE_SIZE][PUZZLE_SIZE];
+	HshufflePuzzle(puzzle);
+
+	int row, col;
+	for (int r = 0; r < PUZZLE_SIZE; r++) {
+		for (int c = 0; c < PUZZLE_SIZE; c++) {
+			if (puzzle[r][c] == 0) {
+				row = r;
+				col = c;
+			}
+		}
+	}
+
+	int key = 0;
+
+	while (!HisEnding(puzzle)) {
+		HprintPuzzle(puzzle);
+		printf("\n\n\n\n\n");
+		printf(">> 방향키 선택\n");
+		key = HgetDirectionKey();
+
+		switch (key) {
+		case RIGHT:
+			if (col > 0) {
+				puzzle[row][col] = puzzle[row][col - 1];
+				puzzle[row][col - 1] = 0;
+				col--;
+			}
+			break;
+		case LEFT:
+			if (col < PUZZLE_SIZE - 1) {
+				puzzle[row][col] = puzzle[row][col + 1];
+				puzzle[row][col + 1] = 0;
+				col++;
+			}
+			break;
+		case UP:
+			if (row < PUZZLE_SIZE - 1) {
+				puzzle[row][col] = puzzle[row + 1][col];
+				puzzle[row + 1][col] = 0;
+				row++;
+			}
+			break;
+		case DOWN:
+			if (row > 0) {
+				puzzle[row][col] = puzzle[row - 1][col];
+				puzzle[row - 1][col] = 0;
+				row--;
+			}
+			break;
+		}
+	}
+
+
+	return 0;
+}
+
+void explainNPuzzle() {
+
+}
+//n*n퍼즐 함수 끝
